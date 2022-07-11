3eefcb3eb2f281bdba48c69e956420bf35c0254b
kleigh23 <rob22003@byui.edu>
2022-07-11 11:41:53 -0600

    removed actor loop

--- cycle/game/scripting/move_actors_action.py
+++ cycle/game/scripting/move_actors_action.py
@@ -16,9 +16,9 @@ class MoveActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        actors = cast.get_all_actors()
-        for actor in actors:
-            actor.move_next()
+        # actors = cast.get_all_actors()
+        # for actor in actors:
+        #     actor.move_next()
 
         snake = cast.get_first_actor("snakes")
         snake2 = cast.get_second_actor("snakes")
