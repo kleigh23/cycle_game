6e4fb843fa0fe058d19e23510620c8b0d03a748b
Shannapedia <96964900+Shannapedia@users.noreply.github.com>
2022-07-11 13:00:18 -0600

    changed the position of the snakes

--- cycle/game/casting/snake.py
+++ cycle/game/casting/snake.py
@@ -110,7 +110,7 @@ class Snake(Actor):
                 segment.set_color(color)
                 self._segments.append(segment)
         else:
-            x = 600
+            x = 900
             y = int(constants.MAX_Y / 2)
 
             for i in range(constants.SNAKE_LENGTH):
